c6decff3752e2d3cdb47220b17b092a7c3113980
Alexandr <jwchaganav@gmail.com>
2023-02-12 17:32:10 +0400

    ButtonOneHandler

--- src/main/java/org/example/models/ButtonOneHandler.java
+++ src/main/java/org/example/models/ButtonOneHandler.java
@@ -2,5 +2,15 @@ package org.example.models;
 import java.awt.*;
 import java.awt.event.*;
 
-public class ButtonOneHandler {
+public class ButtonOneHandler implements ActionListener{
+    private ButtonPanel BP;
+    private PlotPanel PP;
+    public ButtonOneHandler(ButtonPanel bp, PlotPanel pp){
+        this.BP = bp;
+        this.PP = pp;
+    }
+    public void actionPerformed(ActionEvent ae){ // обработка нажатия кнопки
+        PP.G = PP.G.remember(BP);
+        PP.G.plot(PP.getGraphics());
+    }
 }
