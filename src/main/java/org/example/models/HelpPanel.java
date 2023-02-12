5699ce613bf856c76c8993fc5e18d31929f8ef28
Alexandr <jwchaganav@gmail.com>
2023-02-12 17:24:02 +0400

    HelpPanel

--- src/main/java/org/example/models/HelpPanel.java
+++ src/main/java/org/example/models/HelpPanel.java
@@ -2,5 +2,20 @@ package org.example.models;
 import java.awt.*;
 import java.awt.event.*;
 
-public class HelpPanel {
+public class HelpPanel extends Panel{
+    public  Label L;
+    public TextArea TxtA;
+    public HelpPanel(int x, int y, int W, int H){
+        setBackground(Color.LIGHT_GRAY);
+        setBounds(x,y,W,H);
+        setLayout(null);
+        L = new Label("Справка",Label.CENTER);
+        L.setBounds(0,0,W,20);
+        add(L);
+        TxtA = new TextArea(" График функции y(x)=(1+sin(x))/(1+|x|)");
+        TxtA.setFont(new Font("Serif",Font.PLAIN,15));
+        TxtA.setBounds(5,20,W-10,60);
+        TxtA.setEditable(false); // область недоступна для редактирования
+        add(TxtA);
+    }
 }
